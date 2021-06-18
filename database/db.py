ae1d662680d56c3e6a0d9bb0e9b5f4176f6d12e0
Liyina Veizaga <liyina.lva@gmail.com>
2021-06-17 23:42:42 -0400

    Change conection of database

--- database/db.py
+++ database/db.py
@@ -7,23 +7,18 @@ from config_reader import NoveConfiguration
 
 class Database:
 
-
-    #def __repr__(self):
-        #return 'DB(url=%s)' % self._host
-
     def __int__(self):
-        configuration = NoveConfiguration()
+        pass
 
+    def session(self):
+        configuration = NoveConfiguration()
         self._host = configuration.postgres_host
         self._user = configuration.postgres_user
         self._password = configuration.postgres_password
         self._port = configuration.postgres_port
         self._database = configuration.postgres_database
-
-
-    def session(self):
-        self._driver = psycopg2.connect(host=self._host, user=self._user, password=self._password, port=self._port, database=self._database)
-
+        self._driver = psycopg2.connect(host=self._host, user=self._user, password=self._password, port=self._port,
+                                        database=self._database)
         if self._driver:
             print("connection pool created successfully")
-        return self._driver.getconn()
+        return self._driver
